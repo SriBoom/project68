c3b4e10401464ce6023e11f2a51385125942faa6
SriBoom <74517999+SriBoom@users.noreply.github.com>
2021-05-27 18:17:03 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import FacebookScreen from './screens/fb';
+import InstagramScreen from './screens/in';
+
+export default class App extends React.Component() {
+  render(){
+    return (
+        <AppContainer/>
+    );
+  }
+  }
+  
+  
+var TabNavigator = createBottomTabNavigator({
+  Facebook:{screen:FacebookScreen},
+  Instagram:{screen:InstagramScreen},
+})
+const AppContainer = createAppContainer(TabNavigator)
