0251dcea49f2c351e3be0250a8c613da0e57f006
SriBoom <74517999+SriBoom@users.noreply.github.com>
2021-06-06 15:56:19 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
-import TransactionScreen from './screens/BookTransactionScreen';
-import SearchScreen from './screens/SearchScreen';
+import FacebookScreen from './screens/fb';
+import InstagramScreen from './screens/in';
 
-export default class App extends React.Component{
-    render(){
-       return (
-           <AppContainer/>
-       );
-    }
-}
-
-
+export default class App extends React.Component() {
+  render(){
+    return (
+        <AppContainer/>
+    );
+  }
+  }
+  
+  
 var TabNavigator = createBottomTabNavigator({
-    Transaction:{screen:TransactionScreen},
-    Search:{screen:SearchScreen},
-  })
-  const AppContainer = createAppContainer(TabNavigator)
+  Facebook:{screen:FacebookScreen},
+  Instagram:{screen:InstagramScreen},
+})
+const AppContainer = createAppContainer(TabNavigator)
