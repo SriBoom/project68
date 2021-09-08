11a13f8d924c5bd5c73a3f8842d55575d99b3bb1
SriBoom <74517999+SriBoom@users.noreply.github.com>
2021-09-08 14:04:16 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {createBottomTabNavigator} from 'react-navigation-tabs';
 import FacebookScreen from './screens/fb';
 import InstagramScreen from './screens/in';
 
-export default class App extends React.Component() {
+export default class App extends React.Component {
   render(){
     return (
         <AppContainer/>
